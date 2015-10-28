7a335f09f5aa52923a306f5b0a512a34672c9bb4
Joe Fiala <joeafiala@gmail.com>
2015-10-27 17:13:28 -0700

    Work on structs; added a container of structured lines to the part struct

--- Lab2/Lab2/Utility.cpp
+++ Lab2/Lab2/Utility.cpp
@@ -16,3 +16,64 @@ bool utility::doesFileExist(const std::string& file)
 		return false;
 	}
 }
+
+//Trims whitespace from string's beginning & end (see http://stackoverflow.com/questions/1798112/removing-leading-and-trailing-spaces-from-a-string )
+std::string utility::trim(const std::string &str, const std::string &whitespace = " \t")
+{
+	const auto strBegin = str.find_first_not_of(whitespace);
+	if (strBegin == std::string::npos)
+		return "";
+
+	const auto strEnd = str.find_last_not_of(whitespace);
+	const auto strRange = strEnd - strBegin + ONE;
+
+	return str.substr(strBegin, strRange);
+}
+
+part::part(std::string name, std::string file)
+{
+	characterName = name;
+	fileName = file;
+
+	if (!utility::doesFileExist(file))
+	{
+		//problem with retrieving lines
+	}
+	else
+	{
+		// @Elom -- feel free to revise/redo this as you see fit
+		// this is essentially just what I did for Lab 1 to parse
+		// a string into the line # and the line text
+		// if you have a more elegant solution, feel free to 
+		// add that here instead
+		std::ifstream ifs(fileName);
+		std::string line;
+		std::string deli = " "; //used for whitespace delimeter
+
+		while (!ifs.eof() && ifs.good())
+		{
+			getline(ifs, line);
+
+			if (line.find_first_of(deli) == std::string::npos) continue;
+
+			//get line number
+			std::string num = line.substr(ZERO, line.find(deli));
+			int number = atoi(num.c_str());
+			
+			if (number == ZERO) continue; //failed conversion
+
+			//get text
+			line.erase(ZERO, line.find(deli) + deli.length());
+			std::string text = utility::trim(line);
+
+			if (text.empty()) continue;
+
+			//creating the structured line
+			Line tempLine(number, characterName, text);
+			linesOfPart.push_back(tempLine);
+		}
+
+		//setting the iterator to the beginning of the container
+		linesOfPartIterator = linesOfPart.begin();
+	}
+}
--- Lab2/Lab2/Utility.h
+++ Lab2/Lab2/Utility.h
@@ -1,24 +1,40 @@
 #pragma once
 
 #include <string>
+#include <sstream>
 #include <fstream>
+#include "Play.h" //for Line...maybe move that here?
+
+enum numConstants : int
+{
+	ZERO = 0,
+	ONE = 1,
+	TWO = 2,
+};
 
 namespace utility
 {
 	static bool doesFileExist(const std::string& file);
+	static std::string trim(const std::string &str, const std::string &whitespace);
 };
 
 struct part
 {
-
+	part(std::string name, std::string file);
+	std::string characterName;
+	std::string fileName;
+	std::vector<Line> linesOfPart;
+	std::vector<Line>::iterator linesOfPartIterator;
 };
 
 struct fragment
 {
-
+	std::vector<shared_ptr<part>> partContainer;
+	std::vector<shared_ptr<part>>::iterator partContinerIterator;
 };
 
 struct script
 {
-
+	std::vector<shared_ptr<fragment>> fragmentContainer;
+	std::vector<shared_ptr<fragment>>::iterator fragmentContinerIterator;
 };
